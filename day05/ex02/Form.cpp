087ee6bc5a6001737d5955e356cdbf4ab8c71e66
dboyer <dboyer@student.42.fr>
2021-01-12 16:40:10 +0000

    add day05 ex02

---
+++ day05/ex02/Form.cpp
@@ -0,0 +1,113 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Form.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: root <dboyer@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/01/12 15:25:52 by root              #+#    #+#             */
+/*   Updated: 2021/01/12 15:57:36 by root             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Form.hpp"
+
+/******************************************************************************
+ *				Constructors
+ *****************************************************************************/
+Form::Form(std::string const &name, int const gradeToSign,
+		   int const gradeToExec) throw(Form::IGradeException)
+	: _name(name), _isSigned(false), _gradeToSign(gradeToSign),
+	  _gradeToExec(gradeToExec)
+{
+	if (gradeToSign < 1 || gradeToExec < 1)
+	{
+		throw(Form::GradeTooLowException());
+	}
+	else if (gradeToSign > 150 || gradeToExec > 150)
+	{
+		throw(Form::GradeTooHighException());
+	}
+}
+
+Form::Form(Form const &other)
+	: _name(other.getName()), _isSigned(other.isSigned()),
+	  _gradeToSign(other.getGradeToSign()), _gradeToExec(other.getGradeToExec())
+{
+}
+
+Form &Form::operator=(Form const &other)
+{
+	(void)other;
+	return *this;
+}
+
+Form::~Form()
+{
+	std::cout << "Form " << this->_name << " destroyed" << std::endl;
+}
+
+/******************************************************************************
+ *				Getters
+ *****************************************************************************/
+std::string Form::getName() const
+{
+	return this->_name;
+}
+
+bool Form::isSigned() const
+{
+	return this->_isSigned;
+}
+
+int Form::getGradeToExec() const
+{
+	return this->_gradeToExec;
+}
+
+int Form::getGradeToSign() const
+{
+	return this->_gradeToSign;
+}
+
+/******************************************************************************
+ *				Methods
+ *****************************************************************************/
+void Form::beSigned(Bureaucrat const &b) throw(Form::IGradeException)
+{
+	if (b.getGrade() < this->_gradeToSign)
+	{
+		this->_isSigned = true;
+	}
+	else
+	{
+		throw(Form::GradeTooLowException());
+	}
+}
+
+/******************************************************************************
+ *				Exceptions
+ *****************************************************************************/
+const char *Form::GradeTooHighException::what() const throw()
+{
+	return "Grade too high";
+}
+
+const char *Form::GradeTooLowException::what() const throw()
+{
+	return "Grade too low";
+}
+
+bool operator==(Form const &f1, Form const &f2)
+{
+	return f1.getGradeToExec() == f2.getGradeToExec() &&
+		   f1.getGradeToSign() == f2.getGradeToSign() &&
+		   f1.getName() == f2.getName();
+}
+
+std::ostream &operator<<(std::ostream &os, Form const &f)
+{
+	return os << "Name: " << f.getName()
+			  << " Grade to exec :" << f.getGradeToExec()
+			  << " Grade to sign: " << f.getGradeToSign() << std::endl;
+}
---
+++ day05/ex02/ShrubberyCreationForm.hpp
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ShrubberyCreationForm.hpp                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: root <dboyer@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/01/12 16:31:42 by root              #+#    #+#             */
+/*   Updated: 2021/01/12 16:39:40 by root             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef SHRUBBERYCREATION_H
+#define SHRUBBERYCREATION_H
+
+#include "Form.hpp"
+
+class ShrubberyCreationForm public Form
+{
+}
+
+#endif
